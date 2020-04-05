0e842df9b046b166f31122388470f4e5dfd9a440
Spopoi <61315723+Spopoi@users.noreply.github.com>
2020-04-05 16:24:45 +0200

    Update esercizio-c-2020-04-02.c

--- src/esercizio-c-2020-04-02.c
+++ src/esercizio-c-2020-04-02.c
@@ -1,20 +1,12 @@
-//#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#define NUM_CHARS 4
 
-char maxChar(char *array) {
-	char result = array[0];
-	for (int i = 1; i < sizeof(array); i++) {
-		if (result < array[i])
-			result = array[i];
-	}
-	return result;
-}
-
-char minChar(char *array) {
+unsigned char minChar(unsigned char *array) {
 	char result = array[0];
 	for (int i = 1; i < sizeof(array); i++) {
 		if (result > array[i])
@@ -23,29 +15,36 @@ char minChar(char *array) {
 	return result;
 }
 
+unsigned char maxChar(unsigned char *array) {
+	char result = array[0];
+	for (int i = 1; i < sizeof(array); i++) {
+		if (result < array[i])
+			result = array[i];
+	}
+	return result;
+}
+
 int main(int argc, char *argv[]) {
 
-#define NUM_CHARS 80
-
-	//mettere array statico
-	char char_array[NUM_CHARS];
-	char inputChar;
+	unsigned char char_array[NUM_CHARS];
+	unsigned char inputChar;
 	int index = 0;
 	int numberOfArray = 0;
+	printf("ciao, inserisci i caratteri: \n");
 
-	while ((inputChar = getchar()) != EOF){
+	while ((inputChar = getchar()) != EOF) {
 
 		char_array[index] = inputChar;
 		index++;
 
-		if (index == NUM_CHARS+1) {
+		if (index == NUM_CHARS) {
 			numberOfArray++;
 			switch (fork()) {
 			case 0:
 				printf("sono il processo figlio\n");
-				char minchar = minChar(char_array);
-				printf("il carattere minimo è: %c\n", minchar);
-				char maxchar = maxChar(char_array);
+				unsigned char minchar = minChar(char_array);
+				printf("il carattere minimo è: %d\n", minchar);
+				unsigned char maxchar = maxChar(char_array);
 				printf("il carattere massimo è: %c\n", maxchar);
 
 				exit(0);
@@ -57,28 +56,15 @@ int main(int argc, char *argv[]) {
 				break;
 			default:
 
-				printf("sono il processo padre\n");
-
 				if (wait(NULL) == -1) {
 					perror("wait error");
-				} else {
-					printf(
-							"[parent]il processo figlio ha terminato! ora termino anche io.\n");
 				}
 				index = 0;
 				memset(char_array, 0, NUM_CHARS * sizeof(char));
-				printf("array %d con contenuto al posto i di = %d\n",
-						numberOfArray, char_array[3]);
-
-				//	exit(0);
-				// quando il processo padre termina, il processo figlio diventa
-				// "orfano" e viene "adottato" dal processo init,
-				// che automaticamente invoca wait() per rimuovere gli zombie.
 			}
-			/*index = 0;
-			memset(char_array, 0, NUM_CHARS * sizeof(char));*/
 		}
 	}
 
-	printf("%d\n", numberOfArray);
+	printf("ho creato %d figli\n", numberOfArray);
+	return 0;
 }
